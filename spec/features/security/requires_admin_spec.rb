2470ec14540632662c7a69f2b9a59d9d46cba2d8
Daniel Fone <daniel@fone.net.nz>
2013-03-19 13:17:57 +1300

    Fix admin spec

--- spec/features/security/requires_admin_spec.rb
+++ spec/features/security/requires_admin_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 feature "Requires admin" do
 
   scenario 'non-admin user visits' do
-    expect { visit root_path protocol: 'http', admin: false }.to raise_error ActionController::RoutingError
+    expect { visit root_url protocol: 'https', admin: false }.to raise_error ActionController::RoutingError
   end
 
 end
