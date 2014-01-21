639a55a3bd6348298e3b0b5c89830d5543c7d2bd
Daniel Fone <daniel@fone.net.nz>
2014-01-22 10:58:09 +1300

    Tidy dummy application for spec

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -1,7 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
 require "action_controller/railtie"
-require "sprockets/railtie"
 
 Bundler.require
 require "heroku_config_vars"
@@ -44,6 +43,35 @@ module Dummy
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    # Dummy secrect token
+    config.secret_token = 'x' * 30
+
+    # The test environment is used exclusively to run your application's
+    # test suite. You never need to work with it otherwise. Remember that
+    # your test database is "scratch space" for the test suite and is wiped
+    # and recreated between test runs. Don't rely on the data there!
+    config.cache_classes = true
+
+    # Configure static asset server for tests with Cache-Control for performance
+    config.serve_static_assets = true
+    config.static_cache_control = "public, max-age=3600"
+
+    # Log error messages when you accidentally call methods on nil
+    config.whiny_nils = true
+
+    # Show full error reports and disable caching
+    config.consider_all_requests_local       = true
+    config.action_controller.perform_caching = false
+
+    # Raise exceptions instead of rendering exception templates
+    config.action_dispatch.show_exceptions = false
+
+    # Disable request forgery protection in test environment
+    config.action_controller.allow_forgery_protection    = false
+
+    # Print deprecation notices to the stderr
+    config.active_support.deprecation = :stderr
   end
 end
 
