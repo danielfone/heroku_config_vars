a323c9f4a8d614d394269ba5cf546952c2d12c60
Daniel Fone <daniel@fone.net.nz>
2014-01-22 11:04:55 +1300

    Remove unnecessary routes from dummy app

--- spec/dummy/app/controllers/application_controller.rb
+++ spec/dummy/app/controllers/application_controller.rb
@@ -1,18 +1,9 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
 
-  before_filter :break_inheritance
+protected
 
-  protected
-
-    def heroku_config_auth?
-      params[:admin] != 'false'
-    end
-
-  private
-
-    def break_inheritance
-      main_app.widgets_path
-    end
+  def heroku_config_auth?
+    params[:admin] != 'false'
+  end
 
 end
--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-
-  resources :widgets
-
   mount HerokuConfigVars::Engine => "/heroku_config_vars"
-
 end
